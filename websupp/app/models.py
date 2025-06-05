4c5881feb8ecd26e4eca4c7d754ef6da1443e11c
Lê Văn Đạt <datbn09112004@gmail.com>
2025-06-05 09:50:59 +0700

    done

--- websupp/app/models.py
+++ websupp/app/models.py
@@ -36,7 +36,7 @@ class Order(models.Model):
     note = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     status = models.CharField(default="Đang xử lý", max_length=50)
-    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     payment_method = models.CharField(max_length=10, choices=PAYMENT_METHODS, default='cod')  # thêm trường này
 
     def __str__(self):
